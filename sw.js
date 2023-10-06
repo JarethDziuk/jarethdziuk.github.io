f8aa59fd15bbc19804a7160b19378cdbd367df1f
Jonathan Dziuk <funal1045@gmail.com>
2023-10-06 15:41:22 -0400

    Downloadable

--- sw.js
+++ sw.js
@@ -7,5 +7,9 @@ self.addEventListener("install", e =>{
 });
 
 self.addEventListener("fetch", e => {
-    console.log('Intercepting fetch request for: ${e.request.url}');
-})
+    e.respondWidth(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
