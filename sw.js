9b067967104f0813494d58027952cac658c1471a
Jonathan Dziuk <funal1045@gmail.com>
2023-10-06 15:34:51 -0400

    instalable

--- sw.js
+++ sw.js
@@ -3,5 +3,9 @@ self.addEventListener("install", e =>{
         caches.open("static").then(cache =>{
             return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
         })
-    )
+    );
 });
+
+self.addEventListener("fetch", e => {
+    console.log('Intercepting fetch request for: ${e.request.url}');
+})
